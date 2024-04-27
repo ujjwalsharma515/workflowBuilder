8783e9918cbfa9322cbd1dec1dbccbf613544276
ujjwal <ujjwalsharma515@gmail.com>
2024-04-27 12:56:14 +0530

    deploy

--- backend/server.js
+++ backend/server.js
@@ -1,12 +1,21 @@
 const express = require('express');
 const app = express();
+const cors = require('cors');
 const mongoose = require("mongoose");
 
-const dotenv = require("dotenv")
+const dotenv = require("dotenv");
+const methods = require('methods');
 app.use(express.json());
 app.use(require("./routes/router"));
 dotenv.config({path:"./.env"});
 const PORT = process.env.PORT || 5000 ;
+app.use(cors(
+    {
+        origin:'http://localhost:3000',
+        methods:["POST","GET"],
+        credentials:true
+    }
+))
 
 //for connect MongoDB server
 const Connect =async()=>{
